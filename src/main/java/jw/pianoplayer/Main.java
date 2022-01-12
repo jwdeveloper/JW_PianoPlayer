2855aebdddd694c785580dc02eee1ae5572d5a00
Jacek Wolniewicz <jacekwoln@gmail.com>
2022-01-12 16:19:34 +0100

    Refactor

--- src/main/java/jw/pianoplayer/Main.java
+++ src/main/java/jw/pianoplayer/Main.java
@@ -3,15 +3,16 @@ package jw.pianoplayer;
 import jw.pianoplayer.services.SettingsService;
 import jw.spigot_fluent_api.dependency_injection.InjectionManager;
 import jw.spigot_fluent_api.fluent_plugin.FluentPlugin;
-import jw.spigot_fluent_api.fluent_plugin.FluentPluginConfiguration;
 import jw.spigot_fluent_api.fluent_plugin.configuration.PluginConfiguration;
+import jw.spigot_fluent_api.fluent_plugin.configuration.config.ConfigFile;
 import jw.spigot_fluent_api.utilites.files.FileUtility;
 
 
 public final class Main extends FluentPlugin {
 
+
     @Override
-    protected void OnConfiguration(PluginConfiguration configuration) {
+    protected void OnConfiguration(PluginConfiguration configuration, ConfigFile configFile) {
         configuration.useDataContext()
                 .useDependencyInjection()
                 .useDebugMode()
